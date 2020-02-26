a8b96b235e5a3dfb4c40ff7ab3ba57247c78f173
Bryan <BryanBorgesano97@gmail.com>
2020-02-26 16:25:45 -0500

    Updates

---
+++ precache-manifest.19dcd1c710b9802f256d7907c6e9bdcd.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9ae8b0692bb7072e517cd2d25130d1f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9795fbab4fe62ee4a6ab",
+    "url": "/portfolio/static/css/main.0e506b7a.chunk.css"
+  },
+  {
+    "revision": "b422d5817f758a022232",
+    "url": "/portfolio/static/js/2.c7e166b3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/portfolio/static/js/2.c7e166b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9795fbab4fe62ee4a6ab",
+    "url": "/portfolio/static/js/main.aa7db969.chunk.js"
+  },
+  {
+    "revision": "38aaa630afa71aa979c8",
+    "url": "/portfolio/static/js/runtime-main.418e5641.js"
+  },
+  {
+    "revision": "9bde80c47d26ad9ad1cc777b039fe42f",
+    "url": "/portfolio/static/media/cpp.9bde80c4.png"
+  },
+  {
+    "revision": "0a76803baf136f33bda173ff086f1041",
+    "url": "/portfolio/static/media/csharp.0a76803b.png"
+  },
+  {
+    "revision": "e4474bd09d05ea48435cce347fe95097",
+    "url": "/portfolio/static/media/farmingdale.e4474bd0.png"
+  },
+  {
+    "revision": "8ec908094ae2c2c7a8bf1c2532d5d7a8",
+    "url": "/portfolio/static/media/html.8ec90809.png"
+  },
+  {
+    "revision": "cd44c78f69aba3f1b18036afa5710ffb",
+    "url": "/portfolio/static/media/java.cd44c78f.png"
+  },
+  {
+    "revision": "1fdd0da5368ff308e475750ee4fe2989",
+    "url": "/portfolio/static/media/linux.1fdd0da5.png"
+  },
+  {
+    "revision": "b031c86c7749b7f8d3376b01f373d6ae",
+    "url": "/portfolio/static/media/liu.b031c86c.png"
+  },
+  {
+    "revision": "540397236c66ee18926006c2865a97f1",
+    "url": "/portfolio/static/media/precipart.54039723.png"
+  },
+  {
+    "revision": "8eefa0a844a19d1fcb7c2894fb332acc",
+    "url": "/portfolio/static/media/py.8eefa0a8.png"
+  },
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "/portfolio/static/media/react.47ce6e77.png"
+  },
+  {
+    "revision": "f437cfb2a3ad7b5ec941fbef6c8cfcfd",
+    "url": "/portfolio/static/media/rms.f437cfb2.jpeg"
+  }
+]);
