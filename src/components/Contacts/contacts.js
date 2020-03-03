425a1d87597948cae7f25bfef5860fcbae5e4ee2
Bryan <BryanBorgesano97@gmail.com>
2020-03-03 15:45:24 -0500

    Added background image. Changed some margins mostly

--- src/components/Contacts/contacts.js
+++ src/components/Contacts/contacts.js
@@ -4,13 +4,13 @@ const contacts = () => {
   return (
     <div className={classes.Contacts}>
       <a href="https://docs.google.com/document/d/1uw6tut4270osFizxSfcC9KS5jC5EYdGptpYSqZ1Hm5g/edit">
-      <i class="fab fa-google-drive fa-2x"></i>
+      <i className="fab fa-google-drive fa-2x"></i>
       </a>
       <a href="https://www.linkedin.com/in/bryan-borgesano/">
-        <i class="fab fa-linkedin fa-2x"></i>
+        <i className="fab fa-linkedin fa-2x"></i>
       </a>
       <a href="https://github.com/BryanBorge">
-        <i class="fab fa-github-square fa-2x"></i>
+        <i className="fab fa-github-square fa-2x"></i>
       </a>
     </div>
   );
--- src/components/Pages/Resume/resume.js
+++ src/components/Pages/Resume/resume.js
@@ -4,15 +4,15 @@ const resume = () => {
     return (
         <div className={classes.Resume}>
          <a href="https://docs.google.com/document/d/1uw6tut4270osFizxSfcC9KS5jC5EYdGptpYSqZ1Hm5g/edit">
-         <i class="fab fa-google-drive fa-4x"></i>
+         <i className="fab fa-google-drive fa-4x"></i>
          <p>Resume</p>
           </a>
           <a href="https://www.linkedin.com/in/bryan-borgesano/">
-            <i class="fab fa-linkedin fa-4x"></i>
+            <i className="fab fa-linkedin fa-4x"></i>
             <p>LinkedIn</p>
           </a>
           <a href="https://github.com/BryanBorge">
-            <i class="fab fa-github-square fa-4x"></i>
+            <i className="fab fa-github-square fa-4x"></i>
             <p>GitHub</p>
           </a>
         </div>
