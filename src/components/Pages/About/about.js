c3c93967ef8fdb33dc1e9f04659465eb97b23375
Bryan Borgesano <BryanBorgesano97@gmail.com>
2020-05-08 21:52:20 -0400

    Changed focus

--- src/components/Pages/About/about.js
+++ src/components/Pages/About/about.js
@@ -8,10 +8,9 @@ const about = () => {
         Aspiring full stack developer with a life long fascination for
         technology. Fueled by a passion for creation and progression, I am eager
         to learn the latest technologies through continued coursework and
-        professional development.
-        I have experience building backend services while studying at SUNY
-        Farmingdale and recently completed an online course where I learned
-        ReactJS.
+        professional development. I have experience building backend services
+        while studying at SUNY Farmingdale and recently completed an online
+        course where I learned ReactJS.
       </p>
       <p>
         Actively looking for a position as an Entry Level Developer and always
@@ -19,10 +18,6 @@ const about = () => {
       </p>
       <p className={classes.focus}>
         Current Focus:{" "}
-        <a href="https://github.com/BryanBorge?utf8=%E2%9C%93&tab=repositories&q=&type=&language=python">
-          Python
-        </a>{" "}
-        -{" "}
         <a href="https://www.udemy.com/course/react-the-complete-guide-incl-redux/">
           React
         </a>
