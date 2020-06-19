0eecca8c17df92f365bfca72d2a03d7018bbf342
Bryan Borgesano <BryanBorgesano97@gmail.com>
2020-06-19 12:04:44 -0400

    Added DevPost to resume links

--- src/components/Pages/Resume/resume.js
+++ src/components/Pages/Resume/resume.js
@@ -1,21 +1,25 @@
-import React from 'react' 
-import classes from './resume.module.css'
+import React from "react";
+import classes from "./resume.module.css";
 const resume = () => {
-    return (
-        <div className={classes.Resume}>
-         <a href="https://docs.google.com/document/d/1uw6tut4270osFizxSfcC9KS5jC5EYdGptpYSqZ1Hm5g/edit">
-         <i className="fab fa-google-drive fa-4x"></i>
-         <p>Resume</p>
-          </a>
-          <a href="https://www.linkedin.com/in/bryan-borgesano/">
-            <i className="fab fa-linkedin fa-4x"></i>
-            <p>LinkedIn</p>
-          </a>
-          <a href="https://github.com/BryanBorge">
-            <i className="fab fa-github-square fa-4x"></i>
-            <p>GitHub</p>
-          </a>
-        </div>
-      );
-    };
-export default resume
+  return (
+    <div className={classes.Resume}>
+      <a href="https://docs.google.com/document/d/1ZaOCL9HNV2RNFFeiTGApiCXvs2RKhgyno198-pxQPgQ/edit?usp=sharing">
+        <i className="fab fa-google-drive fa-4x"></i>
+        <p>Resume</p>
+      </a>
+      <a href="https://www.linkedin.com/in/bryan-borgesano/">
+        <i className="fab fa-linkedin fa-4x"></i>
+        <p>LinkedIn</p>
+      </a>{" "}
+      <a href="https://github.com/BryanBorge">
+        <i className="fab fa-github-square fa-4x"></i>
+        <p>GitHub</p>
+      </a>{" "}
+      <a href="https://devpost.com/BryanBorge?ref_content=user-portfolio&ref_feature=portfolio&ref_medium=global-nav">
+        <i className="fas fa-file-code fa-4x"></i>
+        <p>DevPost</p>
+      </a>
+    </div>
+  );
+};
+export default resume;
