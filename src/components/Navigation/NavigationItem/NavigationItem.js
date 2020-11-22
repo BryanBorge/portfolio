9700f8ebd26e7bab90d3d513cbcefa542db5e63c
Bryan Borgesano <BryanBorgesano97@gmail.com>
2020-11-22 12:02:50 -0500

    Formatted with Prettier

--- src/components/Navigation/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItem/NavigationItem.js
@@ -1,10 +1,17 @@
-import React from "react";
-import {Link} from 'react-scroll'
+import React, { useState } from "react";
+import { Link } from "react-scroll";
 import classes from "./NavigationItem.module.css";
-const NavigationItem = (props) => {
+const NavigationItem = props => {
   return (
     <li className={classes.NavigationItem}>
-      <Link href={props.link} to={props.to} smooth={true}offset={-58} duration={300}className={props.active ? classes.active : null}>
+      <Link
+        href={props.link}
+        to={props.to}
+        exact={true}
+        smooth={true}
+        offset={-58}
+        duration={300}
+        className={props.active ? classes.active : null}>
         {props.children}
       </Link>
     </li>
