92a2e81a4558b0999d78e3042a2d726f630a62d6
Bryan Borgesano <BryanBorgesano97@gmail.com>
2020-11-22 12:02:28 -0500

    Formatted with prettier

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,30 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import NavigationItem from "./NavigationItem/NavigationItem";
 import classes from "./Navigation.module.css";
 
-const navigation = () => {
+const Navigation = () => {
   return (
     <ul className={classes.Navigation}>
-      <NavigationItem to="About">
-        About
-      </NavigationItem>
-      <NavigationItem to="Education">
-        Education
-      </NavigationItem>
-      <NavigationItem to="Experience">
-        Experience
-      </NavigationItem>
-      <NavigationItem to="Skills">
-        Skills
-      </NavigationItem>
-      <NavigationItem to="Projects">
-        Projects
-      </NavigationItem>
-      <NavigationItem to="Resume">
-          Resume
-      </NavigationItem>
+      <NavigationItem to='About'>About</NavigationItem>
+      <NavigationItem to='Education'>Education</NavigationItem>
+      <NavigationItem to='Experience'>Experience</NavigationItem>
+      <NavigationItem to='Skills'>Skills</NavigationItem>
+      <NavigationItem to='Projects'>Projects</NavigationItem>
+      <NavigationItem to='Resume'>Resume</NavigationItem>
     </ul>
   );
 };
 
-export default navigation;
+export default Navigation;
