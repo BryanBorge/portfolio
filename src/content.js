af16136875171c0c5cf0601da48b4321c3258656
Bryan Borgesano <BryanBorgesano97@gmail.com>
2021-01-08 15:56:29 -0500

    Added updated at to the bottom of the page

--- src/content.js
+++ src/content.js
@@ -1,30 +1,31 @@
 import React from "react";
 import Experience from "./components/Pages/Experience/experience";
 import Education from "./components/Pages/Education/education";
-import Header from "./components/Header/header"
-import About from "./components/Pages/About/about"
-import Skills from './components/Pages/Skills/skills'
-import Resume from './components/Pages/Resume/resume'
-import Projects from './components/Pages/Projects/projects'
+import Header from "./components/Header/header";
+import About from "./components/Pages/About/about";
+import Skills from "./components/Pages/Skills/skills";
+import Resume from "./components/Pages/Resume/resume";
+import Projects from "./components/Pages/Projects/projects";
 import classes from "./content.module.css";
 
-
+const lastUpdated = new Date("Fri Jan 08 2021 15:58:27").toLocaleString("en-US");
 
 const content = () => {
   return (
     <div className={classes.maincontent}>
-      <Header title='About' id="About"/>
+      <Header title="About" id="About" />
       <About />
-      <Header title='Education' id="Education"/>
+      <Header title="Education" id="Education" />
       <Education />
-      <Header title='Experience' id="Experience"/>
+      <Header title="Experience" id="Experience" />
       <Experience />
-      <Header title='Skills' id="Skills"/>
+      <Header title="Skills" id="Skills" />
       <Skills />
-      <Header title='Projects' id="Projects"/>
+      <Header title="Projects" id="Projects" />
       <Projects />
-      <Header title='Resume' id="Resume"/>
+      <Header title="Resume" id="Resume" />
       <Resume />
+      <p className={classes.updatedAt}>Last updated: {lastUpdated} </p>
     </div>
   );
 };
