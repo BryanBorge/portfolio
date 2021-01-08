cebfb3901b0337f429a60a827a4a3112c4d926be
Bryan Borgesano <BryanBorgesano97@gmail.com>
2021-01-08 15:57:19 -0500

    Updated raspi project

--- src/components/Pages/Projects/projects.js
+++ src/components/Pages/Projects/projects.js
@@ -16,8 +16,8 @@ const projects = () => {
           "You are already here! This site is the first thing I've built using ReactJS, using some components from MaterialUI. Being hosted on GitHub pages."
         }
         link={"https://github.com/BryanBorge/portfolio"}
-        icon='fab fa-github-square fa-2x'
-        tip='Link to GitHub'
+        icon="fab fa-github-square fa-2x"
+        tip="Link to GitHub"
         stack={["React", "CSS", "MaterialUI"]}
       />
       <Project
@@ -28,19 +28,11 @@ const projects = () => {
         }
         link={"https://devpost.com/software/mask-i"}
         link2={"https://github.com/mask-i"}
-        icon='fas fa-code fa-1x'
-        icon2='fab fa-github-square fa-2x'
-        tip='Link to DevPost'
-        tip2='Link to GitHub'
-        stack={[
-          "React",
-          "RaspberryPi",
-          "IoT",
-          "Python",
-          "Hackathon",
-          "Machine Learning",
-          "MaterialUI",
-        ]}
+        icon="fas fa-code fa-1x"
+        icon2="fab fa-github-square fa-2x"
+        tip="Link to DevPost"
+        tip2="Link to GitHub"
+        stack={["React", "RaspberryPi", "IoT", "Python", "Hackathon", "Machine Learning", "MaterialUI"]}
       />
       <Project
         img={pennAndTellerLogo}
@@ -49,20 +41,20 @@ const projects = () => {
           "I found a site that contains every episode of Penn & Teller's Bullshit. Instead downloading each episode one by one I wrote a  Python script to do it for me. Using Selenium, BeautifulSoup and urllib, this is the solution I came up with."
         }
         link={"https://github.com/BryanBorge/bullshit-downloader"}
-        icon='fab fa-github-square fa-2x'
-        tip='Link to GitHub'
+        icon="fab fa-github-square fa-2x"
+        tip="Link to GitHub"
         stack={["Python", "BeautifulSoup", "urllib", "Selenium", "Web Scraping"]}
       />
       <Project
         img={rpi}
         title={"Remote Temperature Monitor"}
         desc={
-          "Web dashboard to display live data from a RaspberryPi. Sensor data is read from the pi at regular intervals and sent to Dweet.io. The dashboard then updates from Dweet.io for real time data visualization."
+          "Web dashboard to display live data from a RaspberryPi. Sensor data is read from the pi at regular intervals and sent to a Firebase real time database. The dashboard then updates for real time data visualization."
         }
-        link={"https://github.com/BryanBorge/temperature-monitor"}
-        icon='fab fa-github-square fa-2x'
-        tip='Link to GitHub'
-        stack={["Python", "RaspberryPi", "IoT", "Google APIs"]}
+        link={"https://github.com/tent-dashboard"}
+        icon="fab fa-github-square fa-2x"
+        tip="Link to GitHub"
+        stack={["Python", "RaspberryPi", "IoT", "Firebase", "React", "Redux", "MaterialUI"]}
       />
     </div>
   );
