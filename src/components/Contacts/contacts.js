0c6468da77766bd2891d6f6ef6a3d5eefdc9a3b1
Bryan Borgesano <BryanBorgesano97@gmail.com>
2021-01-17 20:27:39 -0500

    Updated contact resume link

--- src/components/Contacts/contacts.js
+++ src/components/Contacts/contacts.js
@@ -3,8 +3,8 @@ import classes from "./contacts.module.css";
 const contacts = () => {
   return (
     <div className={classes.Contacts}>
-      <a href="https://docs.google.com/document/d/1uw6tut4270osFizxSfcC9KS5jC5EYdGptpYSqZ1Hm5g/edit">
-      <i className="fab fa-google-drive fa-2x"></i>
+      <a href="https://docs.google.com/document/d/1ZaOCL9HNV2RNFFeiTGApiCXvs2RKhgyno198-pxQPgQ/edit?usp=sharing">
+        <i className="fab fa-google-drive fa-2x"></i>
       </a>
       <a href="https://www.linkedin.com/in/bryan-borgesano/">
         <i className="fab fa-linkedin fa-2x"></i>
