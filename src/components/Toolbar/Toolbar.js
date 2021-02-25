8252461367dc0183ffee2a4d93d53c416357b30c
Bryan Borgesano <BryanBorgesano97@gmail.com>
2021-02-25 13:39:08 -0500

    Commented out sidebar for now until I find add a new one

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,12 +1,12 @@
 import React from "react";
-import Logo from '../Logo/logo'
+import Logo from "../Logo/logo";
 import Navigation from "../Navigation/Navigation";
-import BarToggle from '../Sidebar/BarToggle/BarToggle'
+import BarToggle from "../Sidebar/BarToggle/BarToggle";
 import classes from "./Toolbar.module.css";
 
 const toolbar = props => (
   <header className={classes.Toolbar}>
-    <BarToggle clicked={props.barToggleClicked}/>
+    {/* <BarToggle clicked={props.barToggleClicked} /> */}
     <div className={classes.Logo}>
       <Logo />
     </div>
