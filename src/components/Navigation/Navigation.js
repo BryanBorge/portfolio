b4d7f997d32548bdb57837649efe6089990a83a3
Bryan Borgesano <BryanBorgesano97@gmail.com>
2021-03-26 12:45:12 -0400

    Removed resume page

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,16 +1,15 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import NavigationItem from "./NavigationItem/NavigationItem";
 import classes from "./Navigation.module.css";
 
 const Navigation = () => {
   return (
     <ul className={classes.Navigation}>
-      <NavigationItem to='About'>About</NavigationItem>
-      <NavigationItem to='Education'>Education</NavigationItem>
-      <NavigationItem to='Experience'>Experience</NavigationItem>
-      <NavigationItem to='Skills'>Skills</NavigationItem>
-      <NavigationItem to='Projects'>Projects</NavigationItem>
-      <NavigationItem to='Resume'>Resume</NavigationItem>
+      <NavigationItem to="About">About</NavigationItem>
+      <NavigationItem to="Education">Education</NavigationItem>
+      <NavigationItem to="Experience">Experience</NavigationItem>
+      <NavigationItem to="Skills">Skills</NavigationItem>
+      <NavigationItem to="Projects">Projects</NavigationItem>
     </ul>
   );
 };
