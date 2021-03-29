2ff0963d8dd1384a06fc7b5380ad4d91493ee3d8
Bryan Borgesano <BryanBorgesano97@gmail.com>
2021-03-29 11:36:57 -0400

    Updated about me paragraph

--- src/components/Pages/About/about.js
+++ src/components/Pages/About/about.js
@@ -9,12 +9,15 @@ const about = () => {
       <p>
         Full stack developer with a life long fascination for technology. Fueled by a passion for creation and
         progression, I am eager to learn the latest technologies through continued coursework and professional
-        development. I've had experience with Object-oriented programming languages (Java, C#, C++) while
-        studying at SUNY Farmingdale and have a current focus on full stack development using the MERN stack.
-        (MongoDB, Express, React/React Native, Node.js)
+        development. Currently specializing in the MERN stack with a solid understanding of noSQL databases. I
+        enjoy using React/React Native with Redux for state management along with Node/Express and MongoDB on
+        the backend. I enjoy building web applications that utilize the most recent practices and tools. I
+        also enjoy builidng{" "}
+        <a href="https://www.wired.co.uk/article/internet-of-things-what-is-explained-iot" target="_blank">
+          IoT
+        </a>{" "}
+        projects with Python and a Raspberry Pi.
       </p>
-      <p className={classes.focus}></p>
-      <p>This site was developed by me and shows some of my background. Welcome!</p>
       <div className={classes.Resume}>
         <div className={classes.links}>
           <a
@@ -22,13 +25,11 @@ const about = () => {
             target="_blank"
             style={{textDecoration: "none"}}>
             <i className="fab fa-linkedin fa-4x"></i>
-            <p style={{fontSize: "12px"}}>LinkedIn</p>
           </a>
         </div>
         <div className={classes.links}>
           <a href="https://github.com/BryanBorge" target="_blank" style={{textDecoration: "none"}}>
             <i className="fab fa-github-square fa-4x"></i>
-            <p style={{fontSize: "12px"}}>GitHub</p>
           </a>
         </div>
       </div>
